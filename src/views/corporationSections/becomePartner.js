4fc174c10a581292a74edbf5466dbcb2a5c8492c
lethanhdatdev <lethanhdatdev>
2021-07-21 17:58:00 +0700

    done repsonsive corporation

--- src/views/corporationSections/becomePartner.js
+++ src/views/corporationSections/becomePartner.js
@@ -4,8 +4,9 @@ import styled from 'styled-components';
 import { Image } from "../../components/common/image"
 import { graphql, useStaticQuery } from "gatsby"
 import { Button } from "../../components/Button"
+import { TitleRes } from '../../components/TitleRes';
 const BecomePartner = () => {
-    const corporationbrief = useStaticQuery(graphql`
+  const corporationbrief = useStaticQuery(graphql`
     query {
         corporationbrief: file(sourceInstanceName: { eq: "product" }, name: { eq: "corporation" }) {
         childImageSharp {
@@ -16,49 +17,54 @@ const BecomePartner = () => {
       }
     }
   `)
-    return (
-        <Section>
-            <Container>
-                <Flex col={2} className="flex-row-reverse d-flex">
-                    <div>
-                        <Image fluid={corporationbrief.corporationbrief.childImageSharp.fluid} />
-                    </div>
-                    <div className="w-100">
-                        <div style={{ color: "#7A7A7A", width: "85%" }}>HaloPesa for corporates (Business Customers) are solutions for businesses that allow organizations to collect funds from their customers on a regular basis through HaloPesa or disburse funds to their customers just by a click of a button. HaloPesa is also a suitable means for Merchants to accept payments (HaloPesa Lipa Hapa) from customers with wallets.</div>
-                        <div>
-                            <Button className="mt-4" name="Become a Partner" />
-                        </div>
-                    </div>
-                </Flex>
-                <Flex col={4}>
-                    <div>
-                        <Number className="col-6">
-                            <div className="number">1,000,000+</div>
-                            <div className="des">Transactions</div>
-                        </Number>
-                    </div>
-                    <div>
-                        <Number className="col-6">
-                            <div className="number">500+</div>
-                            <div className="des">Customers</div>
-                        </Number>
-                    </div>
-                    <div>
-                        <Number className="col-6">
-                            <div className="number">1,000,000+</div>
-                            <div className="des">Transactions</div>
-                        </Number>
-                    </div>
-                    <div>
-                        <Number className="col-6">
-                            <div className="number">500+</div>
-                            <div className="des">Customers</div>
-                        </Number>
-                    </div>
-                </Flex>
-            </Container>
-        </Section>
-    )
+  return (
+    <Section>
+      <Container>
+        <TitleRes
+          className="d-md-none d-block"
+          name="For Corpration"
+          des="HaloPesa for corporates (Business Customers) are solutions for businesses that allow organizations to collect funds from their customers on a regular basis through HaloPesa or disburse funds to their customers just by a click of a button. HaloPesa is also a suitable means for Merchants to accept payments (HaloPesa Lipa Hapa) from customers with wallets.    "
+        />
+        <Flex col={2} className="flex-row-reverse d-md-flex d-block">
+          <div>
+            <Image display smWidth="340px" fluid={corporationbrief.corporationbrief.childImageSharp.fluid} />
+          </div>
+          <div className="w-100">
+            <div className="d-none d-md-block" style={{ color: "#7A7A7A", width: "85%" }}>HaloPesa for corporates (Business Customers) are solutions for businesses that allow organizations to collect funds from their customers on a regular basis through HaloPesa or disburse funds to their customers just by a click of a button. HaloPesa is also a suitable means for Merchants to accept payments (HaloPesa Lipa Hapa) from customers with wallets.</div>
+            <div className="d-flex d-md-block justify-content-center align-items-center">
+              <Button className="mt-md-4 mt-2" name="Become a Partner" />
+            </div>
+          </div>
+        </Flex>
+        <Flex col={4}>
+          <div className="d-md-flex justify-content-center mr-4">
+            <Number className="col-6">
+              <div className="number">1,000,000+</div>
+              <div className="des">Transactions</div>
+            </Number>
+          </div>
+          <div className="d-flex justify-content-center mr-4">
+            <Number className="col-6">
+              <div className="number">500+</div>
+              <div className="des">Customers</div>
+            </Number>
+          </div>
+          <div className="d-md-flex justify-content-center mr-4">
+            <Number className="col-6">
+              <div className="number">1,000,000+</div>
+              <div className="des">Transactions</div>
+            </Number>
+          </div>
+          <div className="d-flex justify-content-center mr-4">
+            <Number className="col-6">
+              <div className="number">500+</div>
+              <div className="des">Customers</div>
+            </Number>
+          </div>
+        </Flex>
+      </Container>
+    </Section>
+  )
 }
 
 export default BecomePartner
@@ -71,8 +77,8 @@ const Flex = styled.div`
   justify-content: start;
   padding-top: 20px;
   @media (max-width: ${props => props.theme.screen.sm}) {
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 32px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
   }
 `
 
@@ -82,9 +88,16 @@ const Number = styled.div`
     font-size: 40.3929px;
     letter-spacing: -1.59256px;
     color: #F36000;
-    margin-top: 30px
+    margin-top: 30px;
+    @media (max-width: ${props => props.theme.screen.xs}) {
+      margin-top: 10px;
+      font-size: 32.3929px;
+    }
   }
   .des {
     font-size: 20.8977px;
+    @media (max-width: ${props => props.theme.screen.xs}) {
+      font-size: 17.8977px;
+    }
   }
 `
--- src/views/corporationSections/howitswork.js
+++ src/views/corporationSections/howitswork.js
@@ -6,6 +6,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import {
     CardSubtitle
 } from 'reactstrap';
+import { TitleRes } from '../../components/TitleRes'
 const HowItsWork = () => {
     const [active, setActive] = useState(0)
     const tabs = [
@@ -34,62 +35,70 @@ const HowItsWork = () => {
     }
   `)
     return (
-        <StyledSection>
-            <Container>
-                <div className="text-center">
-                    <h1>How does it work?</h1>
-                    <div className="w-100 d-flex flex-column justify-content-center align-items-center mt-3">
-                        <div style={{ width: '60%', color: "#7A7A7A" }}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</div>
-                    </div>
+        <ContainerStyled>
+            <TitleRes
+                className="d-block d-md-none"
+                name="How does it work?"
+                des="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+            />
+            <div className="text-center d-none d-md-block">
+                <h1>How does it work?</h1>
+                <div className="w-100 d-flex flex-column justify-content-center align-items-center mt-3">
+                    <div style={{ width: '60%', color: "#7A7A7A" }}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</div>
                 </div>
-                <div className=" d-flex flex-column justify-content-center align-items-center mt-5 mb-5">
-                    <ul className="nav nav-pills mb-3" id="pills-tab">
-                        {tabs.map((x, i) => {
-                            return (
-                                <li key={i.toString()} className="nav-item">
-                                    <button onClick={() => {
-                                        setActive(i)
-                                    }} className={`nav-link ${i === active ? "active" : ""} ${i !== active ? "text-dark" : "text-white"}`} style={{
-                                        marginRight: i === 1 ? "50px" : 0,
-                                        marginLeft: i === 1 ? "50px" : 0
-                                    }} >{x.name}</button>
-                                </li>
-                            )
-                        })}
-                    </ul>
+            </div>
+            <div className=" d-flex flex-column justify-content-center align-items-center mt-md-5 mb-md-5 mt-4 mb-0">
+                <ul className="nav nav-pills mb-3" id="pills-tab">
+                    {tabs.map((x, i) => {
+                        return (
+                            <li key={i.toString()} className="nav-item">
+                                <button onClick={() => {
+                                    setActive(i)
+                                }} className={`nav-link ${i === active ? "active" : ""} ${i !== active ? "text-dark" : "text-white"}`} style={{
+                                    marginRight: i === 1 ? "50px" : 0,
+                                    marginLeft: i === 1 ? "50px" : 0
+                                }} >{x.name}</button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <Flex className="d-flex d-md-grid flex-column" col={2}>
+                <div>
+                    <Image display smWidth="345px" fluid={img.img.childImageSharp.fluid} />
                 </div>
-                <Flex className="" col={2}>
-                    <div>
-                        <Image fluid={img.img.childImageSharp.fluid} />
-                    </div>
-                    <div>
-                        <div styke={{ fontWeight: 900 }}>Customer to Business</div>
-                        <div className="mt-3 mb-3">
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                <div>
+                    <div className="text" styke={{ fontWeight: 900 }}>Customer to Business</div>
+                    <div className="mt-3 mb-3 ">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                        </div>
-                        <div className="d-flex justify-content-start align-items-center shadow p-2 rounded">
-                            <div className="m-3 mb-0">
-                                <Image fluid={img.pdf.childImageSharp.fluid} width="60px" />
-                            </div>
-                            <CardSubtitle tag="h6" className="mb-2 text-muted" >Lorem ipsum dolor sit amet, consectetur.pdf</CardSubtitle>
+                    <div className="d-flex justify-content-start align-items-center shadow p-2 rounded">
+                        <div className="m-3 mb-0">
+                            <Image display smWidth="40px" fluid={img.pdf.childImageSharp.fluid} width="60px" />
                         </div>
+                        <CardSubtitle tag="h6" className="text-muted" >Lorem ipsum dolor sit amet, consectetur.pdf</CardSubtitle>
                     </div>
-                </Flex>
-            </Container>
-        </StyledSection>
+                </div>
+            </Flex>
+        </ContainerStyled>
     )
 }
 
 export default HowItsWork
-
-const StyledSection = styled(Section)`
+const ContainerStyled = styled(Container)`
     .active {
         border-radius: 23px;
         background: #FF6E2C!important;
         color: white;
         font-weight: bold
     }
+    .text { 
+        font-size: 30px;
+        line-height: 30px;
+        font-weight: bold;
+    }
 `
+
 
 const Flex = styled.div`
     display: grid;
@@ -99,6 +108,6 @@ const Flex = styled.div`
     padding-top: 20px;
     @media (max-width: ${props => props.theme.screen.sm}) {
         grid-template-columns: 1fr 1fr;
-        grid-gap: 32px;
+        grid-gap: 0px;
     }
 `
