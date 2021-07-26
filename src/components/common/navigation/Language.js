713602c18a70718e45a4c2324820a7a7ad16e894
lethanhdatdev <lethanhdatdev>
2021-07-26 14:49:17 +0700

    done localize

--- src/components/common/navigation/Language.js
+++ src/components/common/navigation/Language.js
@@ -1,62 +1,36 @@
 import React from 'react'
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-    popover: {
-        pointerEvents: 'none',
-    },
-    paper: {
-        padding: theme.spacing(1),
-    },
-}));
-
+import { Link } from 'gatsby';
+import styled from 'styled-components'
+import { Image } from '../image'
+import { graphql, navigate, useStaticQuery } from "gatsby"
+import { changeLocalize, getCurrentLocale } from '../../../utils/localize';
 const Language = () => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const classes = useStyles();
-    const handlePopoverOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handlePopoverClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
-
-    return (<div>
-        <Typography
-            aria-owns={open ? 'mouse-over-popover' : undefined}
-            aria-haspopup="true"
-            onClick={handlePopoverOpen}
-            onMouseLeave={handlePopoverClose}
-        >
-            English
-      </Typography>
-        <Popover
-            id="mouse-over-popover"
-            className={classes.popover}
-            classes={{
-                paper: classes.paper,
-            }}
-            open={open}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-            }}
-            onClose={handlePopoverClose}
-            disableRestoreFocus
-        >
-            <Typography>vi</Typography>
-            <Typography>en</Typography>
-        </Popover>
+    const lang = useStaticQuery(graphql`
+    query {
+      file(sourceInstanceName: { eq: "product" }, name: { eq: "lang" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+    return (<div className=" d-flex flex-row pl-3 pr-3">
+        <Image display smWidth="18px" className="d-flex justify-content-center align-items-center" width="20px" fluid={lang.file.childImageSharp.fluid} />
+        <div className="mr-2">
+            <LinkStyle onClick={() => changeLocalize()} lang={getCurrentLocale() === "en"} first className="english" to="/">English</LinkStyle>
+        </div>
+        <div>
+            <LinkStyle onClick={() => changeLocalize()} lang={getCurrentLocale() === "vi"} to="/">Vietnamese</LinkStyle>
+        </div>
     </div>)
 }
 
 export default Language
+
+const LinkStyle = styled(Link)`
+    color: ${props => props.lang ? "primary" : "white"};
+    padding-right: ${props => props.first ? "10px" : 0};
+    padding-left: ${props => props.first ? "5px" : 0}
+`
